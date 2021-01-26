43ddfb951f019c97df8933bf9f8ad275a99085d1
ZhangYanTJU <zytju@tju.edu.cn>
2021-01-26 15:30:08 +0100

    format

--- fitData_Cantera.py
+++ fitData_Cantera.py
@@ -59,8 +59,8 @@ def write(gas, thermo_fileName_new, plot):
 
             if (nasa_coeffs[0] != Tcommon):
                 print("fitting species: %s"%(species_name), "from ",nasa_coeffs[0], "to ",Tcommon)
-                cp_low = [species.thermo.cp(T)/molecular_weight for T in T_low] #  [J/(kmol·K)] / [kg/kmol] = [J/(kg·K)]
-                cp_high = [species.thermo.cp(T)/molecular_weight for T in T_high] # J/kg
+                cp_low = [species.thermo.cp(T)/molecular_weight for T in T_low] # [J/(kmol·K)] / [kg/kmol] = [J/(kg·K)]
+                cp_high = [species.thermo.cp(T)/molecular_weight for T in T_high] # [J/(kmol·K)] / [kg/kmol] = [J/(kg·K)]
 
                 cp_low_polynomials = np.polyfit(T_low, cp_low, 4)
                 cp_high_polynomials = np.polyfit(T_high, cp_high, 4)
@@ -86,38 +86,38 @@ def write(gas, thermo_fileName_new, plot):
 
                 if plot:
                     Tlist = np.concatenate([T_low, T_high])
-                    cp = [species.thermo.cp(T)/molecular_weight for T in Tlist] # J/kg
-                    cp_fit_low = [ np.sum(np.array([a1_a5_low[i]*T**(i) for i in range(5)])) for T in T_low]
-                    cp_fit_high = [ np.sum(np.array([a1_a5_high[i]*T**(i) for i in range(5)])) for T in T_high]
-                    cp_fit = np.concatenate([cp_fit_low, cp_fit_high])
+                    cp = [species.thermo.cp(T)/molecular_weight for T in Tlist] # [J/(kg·K)]
+                    cp_fit_low = [ np.sum(np.array([a1_a5_low[i]*T**(i) for i in range(5)])) for T in T_low] # cp/R
+                    cp_fit_high = [ np.sum(np.array([a1_a5_high[i]*T**(i) for i in range(5)])) for T in T_high] # cp/R
+                    cp_fit = np.concatenate([cp_fit_low, cp_fit_high])*R
 
                     a1_a5_org_low = species.thermo.coeffs[8:13]
                     a1_a5_org_high = species.thermo.coeffs[1:6]
-                    cp_bad_low = [ np.sum(np.array([a1_a5_org_low[i]*T**(i) for i in range(5)])) for T in T_low]
-                    cp_bad_high = [ np.sum(np.array([a1_a5_org_high[i]*T**(i) for i in range(5)])) for T in T_high]
-                    cp_bad = np.concatenate([cp_bad_low, cp_bad_high])
+                    cp_bad_low = [ np.sum(np.array([a1_a5_org_low[i]*T**(i) for i in range(5)])) for T in T_low] # cp/R
+                    cp_bad_high = [ np.sum(np.array([a1_a5_org_high[i]*T**(i) for i in range(5)])) for T in T_high] # cp/R
+                    cp_bad = np.concatenate([cp_bad_low, cp_bad_high])*R
 
 
                     enthalpy = [species.thermo.h(T)/molecular_weight for T in Tlist] # J/kg
-                    enthalpy_fit_low = [ a6_low + np.sum(np.array([a1_a5_low[i]*T**(i+1)/(i+1) for i in range(5)])) for T in T_low]
-                    enthalpy_fit_high = [ a6_high + np.sum(np.array([a1_a5_high[i]*T**(i+1)/(i+1) for i in range(5)])) for T in T_high]
-                    enthalpy_fit = np.concatenate([enthalpy_fit_low, enthalpy_fit_high])
+                    enthalpy_fit_low = [ a6_low + np.sum(np.array([a1_a5_low[i]*T**(i+1)/(i+1) for i in range(5)])) for T in T_low] # h/R
+                    enthalpy_fit_high = [ a6_high + np.sum(np.array([a1_a5_high[i]*T**(i+1)/(i+1) for i in range(5)])) for T in T_high] # h/R
+                    enthalpy_fit = np.concatenate([enthalpy_fit_low, enthalpy_fit_high])*R
 
                     fig = plt.figure()
 
                     ax1 = fig.add_subplot()
                     ax1.scatter(Tlist, cp, c='blue', label="cp_org, Tcommon=%d"%(species.thermo.coeffs[0]))
-                    ax1.plot(Tlist, cp_fit*R, c='red', label="cp_fit, Tcommon=1000")
-                    ax1.plot(Tlist, cp_bad*R, c='purple', label="cp_bad")
+                    ax1.plot(Tlist, cp_fit, c='red', label="cp_fit, Tcommon=1000")
+                    ax1.plot(Tlist, cp_bad, c='purple', label="cp_bad")
                     ax1.set_xlabel("T [K]")
                     ax1.set_ylabel("cp [J/kg]")
 
                     ax2 = plt.twinx()
                     ax2.scatter(Tlist, enthalpy, c='black', label="h_org, Tcommon=%d"%(species.thermo.coeffs[0]))
-                    ax2.plot(Tlist, enthalpy_fit*R, c='green', label="h_fit, Tcommon=1000")
+                    ax2.plot(Tlist, enthalpy_fit, c='green', label="h_fit, Tcommon=1000")
                     ax2.set_ylabel("h [J/kg]")
 
-                    fig.legend(loc="upper left", bbox_to_anchor=(0,1), bbox_transform=ax1.transAxes)
+                    fig.legend(loc="lower right", bbox_to_anchor=(1,0), bbox_transform=ax1.transAxes)
                     plt.savefig("%s.png"%(species_name), dpi=500, bbox_inches='tight', pad_inches=0.1)
                     plt.close()
 
@@ -125,7 +125,7 @@ def write(gas, thermo_fileName_new, plot):
 
             species_comp = ''
             for atom in species.composition:
-                species_comp += '{:<3}'.format(atom)
+                species_comp += '{:<3}'.format(atom.upper())
                 species_comp += '{:<2}'.format(str(int(species.composition[atom])))
 
             species_phase = 'G'
